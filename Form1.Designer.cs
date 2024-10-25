6e6ffc8118ec80f7dcccc3b0302f28514806f162
itprocessor75 <itprocessor75@gmail.com>
2024-10-25 08:00:48 +0800

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,380 @@
+﻿namespace FoodPlan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tgv_Users = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SurName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Address = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Phone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Height = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Weight = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TypeOfJob = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_Name = new System.Windows.Forms.TextBox();
+            this.txt_SurName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_Address = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_Phone = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_Email = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.num_Height = new System.Windows.Forms.NumericUpDown();
+            this.num_Weight = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_BodyFat = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txt_TypeOfJob = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btn_Clear = new System.Windows.Forms.Button();
+            this.btn_Save = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.tgv_Users)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_Height)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_Weight)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tgv_Users
+            // 
+            this.tgv_Users.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tgv_Users.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.tName,
+            this.SurName,
+            this.Address,
+            this.Phone,
+            this.Email,
+            this.Height,
+            this.Weight,
+            this.TypeOfJob});
+            this.tgv_Users.Location = new System.Drawing.Point(12, 15);
+            this.tgv_Users.MultiSelect = false;
+            this.tgv_Users.Name = "tgv_Users";
+            this.tgv_Users.ReadOnly = true;
+            this.tgv_Users.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.tgv_Users.Size = new System.Drawing.Size(956, 357);
+            this.tgv_Users.TabIndex = 0;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            // 
+            // tName
+            // 
+            this.tName.HeaderText = "Name";
+            this.tName.Name = "tName";
+            this.tName.ReadOnly = true;
+            // 
+            // SurName
+            // 
+            this.SurName.HeaderText = "Sur Name";
+            this.SurName.Name = "SurName";
+            this.SurName.ReadOnly = true;
+            // 
+            // Address
+            // 
+            this.Address.HeaderText = "Address";
+            this.Address.Name = "Address";
+            this.Address.ReadOnly = true;
+            // 
+            // Phone
+            // 
+            this.Phone.HeaderText = "Phone Number";
+            this.Phone.Name = "Phone";
+            this.Phone.ReadOnly = true;
+            // 
+            // Email
+            // 
+            this.Email.HeaderText = "Email";
+            this.Email.Name = "Email";
+            this.Email.ReadOnly = true;
+            // 
+            // Height
+            // 
+            this.Height.HeaderText = "Height";
+            this.Height.Name = "Height";
+            this.Height.ReadOnly = true;
+            // 
+            // Weight
+            // 
+            this.Weight.HeaderText = "Weight";
+            this.Weight.Name = "Weight";
+            this.Weight.ReadOnly = true;
+            // 
+            // TypeOfJob
+            // 
+            this.TypeOfJob.HeaderText = "Type Of Job";
+            this.TypeOfJob.Name = "TypeOfJob";
+            this.TypeOfJob.ReadOnly = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(113, 428);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name:";
+            // 
+            // txt_Name
+            // 
+            this.txt_Name.Location = new System.Drawing.Point(178, 425);
+            this.txt_Name.Name = "txt_Name";
+            this.txt_Name.Size = new System.Drawing.Size(269, 25);
+            this.txt_Name.TabIndex = 2;
+            // 
+            // txt_SurName
+            // 
+            this.txt_SurName.Location = new System.Drawing.Point(178, 467);
+            this.txt_SurName.Name = "txt_SurName";
+            this.txt_SurName.Size = new System.Drawing.Size(269, 25);
+            this.txt_SurName.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(89, 470);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Sur Name:";
+            // 
+            // txt_Address
+            // 
+            this.txt_Address.Location = new System.Drawing.Point(178, 510);
+            this.txt_Address.Name = "txt_Address";
+            this.txt_Address.Size = new System.Drawing.Size(269, 25);
+            this.txt_Address.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(100, 513);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Address:";
+            // 
+            // txt_Phone
+            // 
+            this.txt_Phone.Location = new System.Drawing.Point(178, 553);
+            this.txt_Phone.Name = "txt_Phone";
+            this.txt_Phone.Size = new System.Drawing.Size(269, 25);
+            this.txt_Phone.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(112, 556);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Phone:";
+            // 
+            // txt_Email
+            // 
+            this.txt_Email.Location = new System.Drawing.Point(612, 425);
+            this.txt_Email.Name = "txt_Email";
+            this.txt_Email.Size = new System.Drawing.Size(269, 25);
+            this.txt_Email.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(547, 428);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Email:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(541, 469);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Height:";
+            // 
+            // num_Height
+            // 
+            this.num_Height.DecimalPlaces = 2;
+            this.num_Height.Location = new System.Drawing.Point(613, 467);
+            this.num_Height.Name = "num_Height";
+            this.num_Height.Size = new System.Drawing.Size(79, 25);
+            this.num_Height.TabIndex = 12;
+            // 
+            // num_Weight
+            // 
+            this.num_Weight.DecimalPlaces = 2;
+            this.num_Weight.Location = new System.Drawing.Point(802, 467);
+            this.num_Weight.Name = "num_Weight";
+            this.num_Weight.Size = new System.Drawing.Size(79, 25);
+            this.num_Weight.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(730, 469);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 17);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Weight:";
+            // 
+            // txt_BodyFat
+            // 
+            this.txt_BodyFat.Location = new System.Drawing.Point(612, 510);
+            this.txt_BodyFat.Name = "txt_BodyFat";
+            this.txt_BodyFat.Size = new System.Drawing.Size(269, 25);
+            this.txt_BodyFat.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(529, 513);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(65, 17);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Body Fat:";
+            // 
+            // txt_TypeOfJob
+            // 
+            this.txt_TypeOfJob.Location = new System.Drawing.Point(613, 554);
+            this.txt_TypeOfJob.Name = "txt_TypeOfJob";
+            this.txt_TypeOfJob.Size = new System.Drawing.Size(269, 25);
+            this.txt_TypeOfJob.TabIndex = 18;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(514, 557);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(80, 17);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Type of Job:";
+            // 
+            // btn_Clear
+            // 
+            this.btn_Clear.Location = new System.Drawing.Point(613, 598);
+            this.btn_Clear.Name = "btn_Clear";
+            this.btn_Clear.Size = new System.Drawing.Size(100, 34);
+            this.btn_Clear.TabIndex = 19;
+            this.btn_Clear.Text = "Clear";
+            this.btn_Clear.UseVisualStyleBackColor = true;
+            this.btn_Clear.Click += new System.EventHandler(this.btn_Clear_Click);
+            // 
+            // btn_Save
+            // 
+            this.btn_Save.Location = new System.Drawing.Point(781, 598);
+            this.btn_Save.Name = "btn_Save";
+            this.btn_Save.Size = new System.Drawing.Size(100, 34);
+            this.btn_Save.TabIndex = 20;
+            this.btn_Save.Text = "Save";
+            this.btn_Save.UseVisualStyleBackColor = true;
+            this.btn_Save.Click += new System.EventHandler(this.btn_Save_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(985, 676);
+            this.Controls.Add(this.btn_Save);
+            this.Controls.Add(this.btn_Clear);
+            this.Controls.Add(this.txt_TypeOfJob);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txt_BodyFat);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.num_Weight);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.num_Height);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txt_Email);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt_Phone);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_Address);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_SurName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_Name);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tgv_Users);
+            this.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tgv_Users)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_Height)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_Weight)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView tgv_Users;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_Name;
+        private System.Windows.Forms.TextBox txt_SurName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_Address;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_Phone;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_Email;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown num_Height;
+        private System.Windows.Forms.NumericUpDown num_Weight;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txt_BodyFat;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txt_TypeOfJob;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btn_Clear;
+        private System.Windows.Forms.Button btn_Save;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SurName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Address;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Phone;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Email;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Height;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Weight;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TypeOfJob;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FoodPlan
+{
+    public partial class Form1 : Form
+    {
+        SQLiteConnection sqlite_conn;
+        List<UserItem> UserList { get; set; } = new List<UserItem>();
+        int selectedUserId = 0;
+        public Form1()
+        {
+            sqlite_conn = new SQLiteConnection("Data Source=foodplan.db");
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Create_Table();
+            LoadUserData();
+        }
+        private void LoadUserData()
+        {
+            UserList = new List<UserItem>();
+            /*tgv_Users.Rows.Clear();*/
+            SQLiteCommand query = sqlite_conn.CreateCommand();
+            try
+            {
+                sqlite_conn.Open();
+                query.CommandText = $"SELECT * FROM users";
+                using (var reader = query.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var id = reader.GetInt32(0);
+                        var name = reader.GetString(1);
+                        var surname = reader.GetString(2);
+                        var address = reader.GetString(3);
+                        var phone = reader.GetString(4);
+                        var email = reader.GetString(5);
+                        var height = reader.GetDecimal(6);
+                        var weight = reader.GetDecimal(7);
+                        var bodyfat = reader.GetString(8);
+                        var typeofjob = reader.GetString(9);
+                        UserList.Add(new UserItem
+                        {
+                            id = id,
+                            name = name,
+                            surname = surname,
+                            address = address,
+                            phone = phone,
+                            email = email,
+                            height = height,
+                            weight = weight,
+                            bodyfat = bodyfat,
+                            typeofjob = typeofjob
+                        });
+                    }
+                }
+                sqlite_conn.Close();
+                tgv_Users.Rows.Clear();
+                foreach(var item in UserList)
+                {
+                    var index = tgv_Users.Rows.Add();
+                    tgv_Users.Rows[index].Cells["ID"].Value = item.id;
+                    tgv_Users.Rows[index].Cells["tName"].Value = item.name;
+                    tgv_Users.Rows[index].Cells["SurName"].Value = item.surname;
+                    tgv_Users.Rows[index].Cells["Address"].Value = item.address;
+                    tgv_Users.Rows[index].Cells["Phone"].Value = item.phone;
+                    tgv_Users.Rows[index].Cells["Email"].Value = item.email;
+                    tgv_Users.Rows[index].Cells["Height"].Value = item.height;
+                    tgv_Users.Rows[index].Cells["Weight"].Value = item.weight;
+                    tgv_Users.Rows[index].Cells["TypeOfJob"].Value = item.typeofjob;
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        private void Create_Table()
+        {
+            try
+            {
+                SQLiteCommand command = sqlite_conn.CreateCommand();
+                sqlite_conn.Open();
+                command.CommandText = "SELECT name FROM sqlite_master WHERE name='users'";
+                var name = command.ExecuteScalar();
+                // check account table exist or not 
+                // if exist do nothing 
+                if (name != null && name.ToString() == "users")
+                {
+                    sqlite_conn.Close();
+                    return;
+                }
+                // acount table not exist, create table and insert 
+                command.CommandText = "CREATE TABLE users (id integer primary key, name VARCHAR(50), sur_name VARCHAR(50), address VARCHAR(100), phone VARCHAR(50), email VARCHAR(50), height real, weight real, body_fat real, type_of_job VARCHAR(100))";
+                command.ExecuteNonQuery();
+                sqlite_conn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void btn_Clear_Click(object sender, EventArgs e)
+        {
+            selectedUserId = 0;
+            txt_Name.Text = "";
+            txt_SurName.Text = "";
+            txt_Address.Text = "";
+            txt_Phone.Text = "";
+            txt_Email.Text = "";
+            num_Height.Value = 0;
+            num_Weight.Value = 0;
+            txt_BodyFat.Text = "";
+            txt_TypeOfJob.Text = "";
+        }
+
+        private void btn_Save_Click(object sender, EventArgs e)
+        {
+            if(txt_Name.Text == null || txt_Name.Text == "")
+            {
+                MessageBox.Show("Please input Name.");
+                return;
+            }
+            if(txt_Email.Text == null || txt_Email.Text == "")
+            {
+                MessageBox.Show("Please input Email.");
+                return;
+            }
+            SQLiteCommand command = sqlite_conn.CreateCommand();
+            if(selectedUserId == 0)
+            {
+                command.CommandText = $"INSERT INTO users (id, name, sur_name, address, phone, email, height, weight, body_fat, type_of_job) VALUES " +
+                    $"(null, '{txt_Name.Text}', '{txt_SurName.Text}', '{txt_Address.Text}', '{txt_Phone.Text}', '{txt_Email.Text}', {num_Height.Value}, {num_Weight.Value}, '{txt_BodyFat.Text}', '{txt_TypeOfJob.Text}')";
+            }
+            else
+            {
+                command.CommandText = $"UPDATE users SET name='{txt_Name.Text}', sur_name='{txt_SurName.Text}', address='{txt_Address.Text}', phone='{txt_Phone.Text}', " +
+                    $"email='{txt_Email.Text}', height={num_Height.Value}, weight={num_Weight.Value}, body_fat='{txt_BodyFat.Text}', type_of_job='{txt_TypeOfJob.Text}' WHERE id={selectedUserId}";
+            }
+            try
+            {
+                sqlite_conn.Open();
+                command.ExecuteNonQuery();
+                sqlite_conn.Close();
+                btn_Clear_Click(new object(), new EventArgs());
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+    public class UserItem
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string address { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+        public decimal height { get; set; }
+        public decimal weight { get; set; }
+        public string bodyfat { get; set; }
+        public string typeofjob { get; set; }
+    }
+}
